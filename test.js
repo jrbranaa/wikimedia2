4dbbbc58b46802dbcc9009c8d8fce970ae764b6d
jrbranaa <jr@ossenx.com>
2016-12-08 08:12:50 -0800

    added mocha

--- test.js
+++ test.js
@@ -3,12 +3,30 @@ const assert = require('assert');
 var webdriver = require('selenium-webdriver');
 var By = require('selenium-webdriver').By;
 var until = require('selenium-webdriver').until;
+var test = require( 'selenium-webdriver/testing' );
 
-var driver = new webdriver.Builder().forBrowser('firefox').build();
-driver.get('https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page');
+test.describe( 'Main page title', function () {
+	var driver;
 
-driver.getTitle().then(function(title) {
-	assert.equal(title,"Wikipedia, the free encyclopedia");
-});
+	test.beforeEach( function () {
+		driver = new webdriver.Builder()
+		.forBrowser( 'firefox' )
+		.build();
+	} );
 
-driver.quit();
+	test.afterEach( function () {
+		driver.quit();
+	} );
+
+	test.it( 'should have correct title', function () {
+		driver.get( 'https://en.wikipedia.beta.wmflabs.org/wiki/Main_Page' );
+		driver.getTitle().then(function(title) {
+			assert.equal(title,"Wikipedia, the free encyclopedia");
+		});
+	} );
+} );
+
+
+
+
+
